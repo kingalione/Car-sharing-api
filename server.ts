c2da6a9884c0f5a814cef6e097e00fbbb0eb2f3e
Ali Karadag <ali.karadag@chronext.com>
2019-08-15 22:06:00 +0200

    try to find some data

--- server.ts
+++ server.ts
@@ -1,21 +1,24 @@
 import { MongoClient } from 'mongodb';
 
-const uri: string =
-  'mongodb+srv://e-go:password4e-go@e-go-project-edbkz.mongodb.net/test?retryWrites=true&w=majority';
+const uri =
+  'mongodb+srv://e-go:password4e-go@e-go-project-edbkz.mongodb.net/E-Go-Project?retryWrites=true&w=majority';
 
-const client: MongoClient = new MongoClient(uri, {
+const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-client.connect(err => {
-  if (err) {
-    console.log(err);
-    return;
-  }
+client.connect((err, db) => {
+  if (err) throw err;
+  var dbo = db.db('e-go_project');
+  var query = { name: 'Titan' };
 
-  const collection = client.db('test').collection('devices');
-  console.log(collection);
-
-  client.close();
+  dbo
+    .collection('cars')
+    .find(query)
+    .toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
 });
